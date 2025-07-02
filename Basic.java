6486fc03f9f00ed7da424630145cc26cd39aeaf2
SaymIslamJihad297 <kojn6193@gmail.com>
2025-07-03 01:36:25 +0600

    Basic Java function and array

---
+++ Basic.java
@@ -0,0 +1,14 @@
+public class Basic{
+    public static void printHelloWorld(String text){
+        System.out.println(text);
+    }
+    public static void printArray(){
+        int marks[] = new int[50];
+        int numbers[] = {1,2, 3};
+        System.out.println(numbers)
+    }
+    public static void main(String args[]){
+        // System.out.println("Hello world");
+        // printHelloWorld("lsjdflskjdf");
+    }
+}
