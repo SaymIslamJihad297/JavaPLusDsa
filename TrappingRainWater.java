b22e0d162e09c89447941941e37acee3f0af243b
SaymIslamJihad297 <kojn6193@gmail.com>
2025-07-04 05:35:56 +0600

    Reverse integer leetcode problem solved

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -25,7 +25,7 @@ public class TrappingRainWater {
         return totalWater;
     }
     public static void main(String[] args) {
-        int height[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int height[] = {1,8,6,2,5,4,8,3,7};
         trapRain(height);
     }
 }
